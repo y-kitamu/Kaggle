d44acb703eb14e7564fdfeb112b650baead817f1
Kitamura <ymyk6602@gmail.com>
2021-01-24 18:23:15 +0900

    fix: bugfix tf (enable gpu in jupyter lab)

--- CassavaLeafDisease/src/__init__.py
+++ CassavaLeafDisease/src/__init__.py
@@ -2,6 +2,11 @@ import os, glob
 
 VERSION = (1, 0, 0)
 
+import tensorflow as tf
+
+for gpu in tf.config.list_physical_devices("GPU"):
+    tf.config.experimental.set_memory_growth(gpu, True)
+
 from src import constant
 from src import dataset
 from src import model
